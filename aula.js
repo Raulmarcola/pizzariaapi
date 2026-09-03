97c6ccf289d614308f83c03030b661e678fbebdf
RAUL MARCOLA <raul.marcola@senacsp.edu.br>
2026-09-03 11:38:33 -0300

    Criação do index e do servidor

---
+++ aula.js
@@ -0,0 +1,3 @@
+export function mensagem(a, cor){
+    console.log(cor(`Bem-vindo(a) a Pizzaria, ${a}`))
+}
---
+++ cardapio.js
@@ -0,0 +1,7 @@
+export let pizzas = [
+    { id: 1, sabor: `Calabresa`, preco: 45.90 },
+    { id: 2, sabor: `Frango`, preco: 55.90 },
+    { id: 3, sabor: `Mussarela`, preco: 40.90 }
+]
+
+// Pizzas é um vetor em que cada valor posicionado é um objeto e vamos exportá-lo
